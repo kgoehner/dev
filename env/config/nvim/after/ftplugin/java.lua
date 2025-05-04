b0196279af2f08645406d4a97f16c33a3b777373
Kyle Goehner <kgoehner@amazon.com>
2025-05-04 15:37:04 -0400

    Update java settings

--- env/config/nvim/after/ftplugin/java.lua
+++ env/config/nvim/after/ftplugin/java.lua
@@ -1,7 +1,7 @@
 local set = vim.opt_local
 
-set.softtabstop = 4 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
-set.shiftwidth = 4 -- size of an indentation (sw).
+set.softtabstop = 2 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
+set.shiftwidth = 2 -- size of an indentation (sw).
 set.expandtab = true -- always uses spaces instead of tab characters (et).
 
 -- helper function for checking if a table contains a value
