c072a9f75644032601db689e6599332c162d9df4
Kyle Goehner <kgoehner@amazon.com>
2026-02-15 08:18:06 -0500

    Update for nvim 11+

--- env/config/nvim/lua/custom/plugins/lsp.lua
+++ env/config/nvim/lua/custom/plugins/lsp.lua
@@ -134,12 +134,10 @@ return {
       })
 
       for server, config in pairs(opts.servers) do
-        -- passing config.capabilities to blink.cmp merges with the capabilities in your
-        -- `opts[server].capabilities, if you've defined it
         config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
         vim.lsp.config(server, config)
-        vim.lsp.enable(server)
       end
+      vim.lsp.enable(vim.tbl_keys(opts.servers))
     end
   }
 }
